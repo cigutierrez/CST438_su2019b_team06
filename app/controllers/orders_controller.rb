c16eea0701078a04b02c8f7c34d3421e522bb38b
Ubuntu <ubuntu@ip-172-31-21-56.us-west-2.compute.internal>
2019-08-14 07:21:45 +0000

    second commit, purchase functionality not working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,36 +35,34 @@ class OrdersController < ApplicationController
         if code != 200
             render json: { error: "Customer email not found. #{ params[:email]}" }, status: 400
             return
-        end 
-    
-    code, item = Item.getItemById (params[:itemId] ) 
-    if code != 200
-      render json: { error: "Item id not found. #{ params[:itemId] }" }, status: 400
-      return
-    end 
-    
-    if item[:stockQty] <= 0
-      render json: { error: "Item is out of stock." } , status: 400
-      return
-    end 
-      
-    @order.itemId = params[:itemId]
-    @order.description = item[:description]
-    @order.customerId = customer[:id]
-    @order.price = item[:price]
-    @order.award = customer[:award]
-    @order.total = @order.price - @order.award
-  
-    if @order.save
-      # put order to customer and item subsystem   
-      # to do their updates
-      code = Customer.putOrder( @order )
-      code = Item.putOrder( @order )
-      render json: @order, status: 201  
-    else
-      render json: @order.errors, status: 400  
+        end
+        
+        code, item = Item.getItemById (params[:itemId] )
+        if code != 200
+            render json: { error: "Item id not found. #{ params[:itemId] }" }, status: 400
+            return
+        end
+        
+        if item[:stockQty] <= 0
+            render json: { error: "Item is out of stock." } , status: 400
+            return
+        end
+        
+        @order.itemId = params[:itemId]
+        @order.description = item[:description]
+        @order.customerId = customer[:id]
+        @order.price = item[:price]
+        @order.award = customer[:award]
+        @order.total = @order.price - @order.award
+        
+        if @order.save
+            # put order to customer and item subsystem
+            # to do their updates
+            code = Customer.putOrder( @order )
+            code = Item.putOrder( @order )
+            render json: @order, status: 201
+        else
+            render json: @order.errors, status: 400
+        end
     end
-   
-  end
-
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -28,5 +28,19 @@ class Customer < ApplicationRecord
             self.lastOrder2 = self.lastOrder
             self.lastOrder = params[:total]
         end
-    end 
+    end
+    
+    def Customer.getCustomerByEmail(email)
+        response = get "/customers?email=#{email}", headers: {  "ACCEPT" => "application/json"}
+            status = response.code
+            customer = JSON.parse response.body, symbolize_names: true
+        # return status code and ruby hash with customer data
+        return status, customer
+    end
+    
+    def Customer.putOrder(order)
+        # do put request to /customers/orders
+        response = put "/customers/order", body:  order.to_json, headers: { "CONTENT_TYPE" => "application/json" }
+        response.code
+    end
 end
